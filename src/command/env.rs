6f56a9be880b09aba808ba55678e2d717e2df160
Chris Czub <chris@penumbralabs.xyz>
2024-07-23 14:59:04 -0400

    bash support env

--- src/command/env.rs
+++ src/command/env.rs
@@ -57,7 +57,35 @@ impl EnvCmd {
     }
 
     fn print_bash(&self, pvm: &Pvm) -> Result<()> {
-        // TODO: use a tera template here
-        unimplemented!("bash env output not implemented yet");
+        let mut context = tera::Context::new();
+
+        if let Some(active_environment) = &pvm.active_environment {
+            context.insert(
+                "pvm_active_environment",
+                &pvm.active_environment
+                    .clone()
+                    .map(|e| e.alias.clone())
+                    .unwrap_or_default(),
+            );
+            if let Some(pcli_home) = pvm.pcli_home() {
+                context.insert("pcli_home", &pcli_home);
+            }
+            if let Some(pclientd_home) = pvm.pclientd_home() {
+                context.insert("pclientd_home", &pclientd_home);
+            }
+            if !active_environment.client_only {
+                if let Some(pd_home) = pvm.pd_home() {
+                    context.insert("pd_home", &pd_home);
+                }
+            }
+        }
+
+        context.insert("path_add", &pvm.path_string());
+
+        let hook_template = include_str!("../../files/bash-env.j2");
+        let hook = tera::Tera::one_off(hook_template, &context, false)?;
+        println!("{}", hook);
+
+        Ok(())
     }
 }
