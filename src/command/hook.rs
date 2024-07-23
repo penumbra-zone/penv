69ac4b787e5f091f788ba5ff4d03f4ffb401cc36
Conor Schaefer <conor@penumbralabs.xyz>
2024-07-23 10:56:06 -0700

    refactor: use enum for shell support
    Tries to implement a TODO about converting the shell reasoning
    to an Enum. Pulled in tera to work with templates, rather than stringing
    together formatted strings manually. Added an as-yet-untested bash hook, too.

--- src/command/hook.rs
+++ src/command/hook.rs
@@ -10,34 +10,63 @@ pub struct HookCmd {
     shell: String,
 }
 
+// Supported shells for configuring pvm environments
+pub enum Shell {
+    Bash,
+    Zsh,
+}
+
+// Implement String -> Shell conversion, to aid in parsing CLI args.
+impl TryFrom<String> for Shell {
+    type Error = anyhow::Error;
+    fn try_from(s: String) -> Result<Shell> {
+        if s == "bash" {
+            Ok(Shell::Bash)
+        } else if s == "zsh" {
+            Ok(Shell::Zsh)
+        } else {
+            anyhow::bail!("unsupported shell: {}", s)
+        }
+    }
+}
+
+// Implement Shell -> String conversion, to aid in looking up relevant configs.
+impl Into<String> for Shell {
+    fn into(self) -> String {
+        match self {
+            Shell::Bash => String::from("bash"),
+            Shell::Zsh => String::from("zsh"),
+        }
+    }
+}
+
 impl HookCmd {
     pub async fn exec(&self, _home: Utf8PathBuf) -> Result<()> {
-        if self.shell != "zsh" {
-            anyhow::bail!("unsupported shell: {}", self.shell);
-        }
-
+        // Look up the path for the currently executed `pvm`, so we can use the fullpath
+        // in the shell configs. This ensures that the sourced shell will work,
+        // even if pvm isn't on PATH already.
         let current_exe = match env::current_exe() {
             Ok(exe_path) => exe_path,
             Err(e) => return Err(anyhow!("failed to get current exe path: {e}")),
         };
 
-        println!(
-            "{}{}{}",
-            r#"
-_pvm_hook() {
-  trap -- '' SIGINT
-  eval "$(""#,
-            current_exe.display(),
-            r#"" env zsh)"
-  trap - SIGINT
-}
-typeset -ag precmd_functions
-if (( ! ${precmd_functions[(I)_pvm_hook]} )); then
-  precmd_functions=(_pvm_hook $precmd_functions)
-fi
-        "#
-        );
+        // Load Tera template config, so we can interpolate pvm fullpath in shell hooks.
+        let mut context = tera::Context::new();
+        context.insert("pvm_executable", &current_exe);
 
+        let shell = Shell::try_from(self.shell.clone())?;
+        match shell {
+            Shell::Zsh => {
+                let hook_template = include_str!("../../files/zsh-hook.j2");
+                let hook = tera::Tera::one_off(hook_template, &context, false)?;
+                println!("{}", hook);
+            }
+            Shell::Bash => {
+                let hook_template = include_str!("../../files/bash-hook.j2");
+                let hook = tera::Tera::one_off(hook_template, &context, false)?;
+                println!("{}", hook);
+            }
+        }
         Ok(())
     }
 }
